6f34d0dc204b100685d5d10fc38f1538239fbdca
phestus <80888394+nestah@users.noreply.github.com>
2023-01-21 14:04:32 +0300

    Update userRouter.js

--- Backend/Routers/userRouter.js
+++ Backend/Routers/userRouter.js
@@ -68,34 +68,7 @@ userRouter.get(
     }
   })
 );
-// userRouter.put(
-//   '/profile',
-//   isAuth,
-//   expressAsyncHandler(async (req, res) => {
-//     const user = await User.findById(req.user._id);
-//     if (user) {
-//       user.name = req.body.name || user.name;
-//       user.email = req.body.email || user.email;
-//       if (user.isSeller) {
-//         user.seller.name = req.body.sellerName || user.seller.name;
-//         user.seller.logo = req.body.sellerLogo || user.seller.logo;
-//         user.seller.description =
-//           req.body.sellerDescription || user.seller.description;
-//       }
-//       if (req.body.password) {
-//         user.password = bcrypt.hashSync(req.body.password, 8);
-//       }
-//       const updatedUser = await user.save();
-//       res.send({
-//         _id: updatedUser._id,
-//         name: updatedUser.name,
-//         email: updatedUser.email,
-//         isAdmin: updatedUser.isAdmin,
-//         token: generateToken(updatedUser),
-//       });
-//     }
-//   })
-// );
+
 
 userRouter.get(
   '/Booking',
